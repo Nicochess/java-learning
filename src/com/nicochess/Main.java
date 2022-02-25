66c2465448076211fc1ef018910166048de2ef3c
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-02-24 23:47:57 -0300

    feat: learning data types

--- src/com/nicochess/Main.java
+++ src/com/nicochess/Main.java
@@ -3,7 +3,11 @@ package com.nicochess;
 public class Main {
 
     public static void main(String[] args) {
-	    int age = 20;
+	    byte age = 20;
+        long viewsCount = 3_321_123_231L;
+        float price = 10.90F;
+        char letter = 'A';
+        boolean isGreen = true;
         System.out.println(age);
     }
 }
