ac1472f476b1d08eada0561f5920d0f94f602645
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-02-25 21:28:56 -0300

    chore: learning more about data types

--- src/com/nicochess/Main.java
+++ src/com/nicochess/Main.java
@@ -1,5 +1,9 @@
 package com.nicochess;
 
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,6 +12,20 @@ public class Main {
         float price = 10.90F;
         char letter = 'A';
         boolean isGreen = true;
-        System.out.println(age);
+
+        String name = "Nicolas Magno";
+        System.out.println(name);
+        System.out.println(name.length());
+        System.out.println(name.endsWith("Magno"));
+        System.out.println(name.indexOf("o"));
+        System.out.println(name.replace("Magno", "Silva"));
+        System.out.println(name.toLowerCase());
+        Date now = new Date();
+        System.out.println(now);
+
+        int[] numbers = new int[5];
+        numbers[0] = 3;
+        Arrays.toString(numbers);
+        System.out.println(numbers);
     }
 }
