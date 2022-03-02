89320db8bc5158c5cac80a33a53a4dd1f63feeb5
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-03-01 21:20:52 -0300

    feat: oop learning

---
+++ src/com/nicochess/Car.java
@@ -0,0 +1,17 @@
+package com.nicochess;
+
+public class Car {
+    String make = "Ford";
+    String model = "Caminhonete";
+    int year = 2020;
+    String color = "Gray";
+    double price = 200_000.00;
+
+    public void drive(){
+        System.out.println("You are driving.");
+    }
+
+    public void brake(){
+        System.out.println("You step the brakes.");
+    }
+}
--- src/com/nicochess/Main.java
+++ src/com/nicochess/Main.java
@@ -1,27 +1,44 @@
 package com.nicochess;
 
-import java.text.DecimalFormat;
-import java.util.Scanner;
+import javax.swing.*;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
+        String name = JOptionPane.showInputDialog("Qual seu nome?");
+        JOptionPane.showMessageDialog(null, "Olá, "+name);
 
         while (true){
-            System.out.print("Number: ");
-            int number = input.nextInt();
+            int number = Integer.parseInt(JOptionPane.showInputDialog("Digite um número:"));
 
             if (number == 0)
                 break;
             else if (number % 5 == 0 && number % 3 == 0)
-                System.out.println("FizzBuzz");
+                JOptionPane.showMessageDialog(null,"FizzBuzz");
             else if (number % 5 == 0)
-                System.out.println("Fizz");
+                JOptionPane.showMessageDialog(null,"Fizz");
             else if (number % 3 == 0)
-                System.out.println("Buzz");
+                JOptionPane.showMessageDialog(null,"Buzz");
             else
-                System.out.println(number);
+                JOptionPane.showMessageDialog(null, number);
         }
+
+        ArrayList<String> foods = new ArrayList<String>();
+
+        foods.add("maça");
+        foods.add("batata");
+
+        System.out.println(foods);
+
+        Car myCar = new Car();
+        System.out.println(myCar.color);
+
+        Human mySelf = new Human("Nicolas", 20, 65);
+    }
+
+    static void userId(String name, Integer age){
+        JOptionPane.showMessageDialog(null, "Oi, sou "+name+"e tenho "+age);
     }
 }
