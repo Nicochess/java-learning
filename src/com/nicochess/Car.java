eb8986eb5aed556bc93ddb57c5e27be4b8c8b2cf
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-03-03 20:44:45 -0300

    feat: to string method implicity

--- src/com/nicochess/Car.java
+++ src/com/nicochess/Car.java
@@ -14,4 +14,8 @@ public class Car {
     public void brake(){
         System.out.println("You step the brakes.");
     }
+
+    public String toString(){
+        return make +"\n"+model+"\n"+year+"\n"+price;
+    }
 }
